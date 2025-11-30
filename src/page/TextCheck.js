52e951ffffc9c08e185fe959cf08d9107ae5bdcc
jainshab1803 <abhijain1803g@gmail.com>
2025-12-01 00:55:12 +0530

    render

--- src/page/TextCheck.js
+++ src/page/TextCheck.js
@@ -17,7 +17,7 @@ const TextCheck = () => {
 
     // Keep original logic
     try {
-      const res = await axios.post("https://backend-production-4e61.up.railway.app/api/text/verify", { text });
+      const res = await axios.post("https://backend-apuv.onrender.com/api/text/verify", { text });
       console.log(res)
 
       saveHistory({
