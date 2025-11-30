e490239c4781b66fb34efd83d853da74d7201834
jainshab1803 <abhijain1803g@gmail.com>
2025-12-01 00:07:58 +0530

    final

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "backend-production-4e61.up.railway.app/api",
+  baseURL: "https://backend-production-4e61.up.railway.app/api",
 });
